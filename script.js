0d362f5342d0f98f92b48263ac82eaf0b31ff8f1
Srikanth-235 <161904315+Srikanth-235@users.noreply.github.com>
2025-06-21 11:15:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-document.getElementById("fname").addEventListener("input",()=>{
+document.getElementById('fname').addEventListener('input',function(){
 	this.value=this.value.toUpperCase();
 });
